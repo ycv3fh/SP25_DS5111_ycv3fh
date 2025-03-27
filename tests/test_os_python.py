290fa5a59be0674f387a4d60ab39d9fbff5a20de
ycv3fh <ycv3fh@Virginia.edu>
2025-03-27 22:15:30 +0000

    update test_os_python.py

--- tests/test_os_python.py
+++ tests/test_os_python.py
@@ -1,14 +1,13 @@
 import platform
 import sys
 
-# Check OS
+#Check  OS
 if platform.system() != "Linux":
     print("Error: This script must be run on Linux.")
     sys.exit(1)
 
 # Check Python version
-if sys.version_info.major != 3 or sys.version_info.minor not in [10, 11]:
-    print(f"Error: Python version is {sys.version_info.major}.{sys.version_info.minor}. Desired versions: 3.10, 3.11.")
-    sys.exit(1)
+def test_python_version():
+    assert sys.version_info.major == 3, "expecting major version 3"
+    assert sys.version_info.minor in [12, 13], "expecting minor in 10 or 11]"
 
-print("OS and Python version checks passed.")
