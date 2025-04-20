f9757603a3b42760df40ae941ddd38b3c2c4622c
ycv3fh <ycv3fh@Virginia.edu>
2025-04-20 15:26:36 +0000

    yahoo.py: added import pandas

--- bin/gainers/yahoo.py
+++ bin/gainers/yahoo.py
@@ -6,6 +6,7 @@ Classes:
     GainerProcessYahoo (Yahoo-specific implementation of GainerProcess)
 '''
 import os
+import padas as pd
 from .base import GainerDownload, GainerProcess
 import datetime
 
