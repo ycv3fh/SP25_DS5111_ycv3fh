37dd8f29ec81036849bc15e49e61e92b8bb6d357
ycv3fh <ycv3fh@Virginia.edu>
2025-04-20 17:33:32 +0000

    edit test_gainers file

--- tests/test_gainers_pipeline.py
+++ tests/test_gainers_pipeline.py
@@ -1,21 +1,11 @@
-<<<<<<< HEAD
-
 import pandas as pd
 import sys
 sys.path.append('.')
 from bin.gainers.yahoo import GainerDownloadYahoo
 from bin.gainers.wsj import GainerDownloadWSJ
-=======
-import sys
-sys.path.append('.')
-from bin.gainers.yahoo import get_yahoo_gainers
-from bin.gainers.wsj import get_wsj_gainers
->>>>>>> fa44f416df49de1a81d577f446138af5685a2c8c
-from bin.normalize_csv import normalize_csv
 
-def test_pipeline_yahoo_data():
+def test_yahoo_pipeline():
 
-<<<<<<< HEAD
     # Create Yahoo-specific processor using your factory
     print("Initializing Yahoo gainer processor...")
     x = factory.GainerFactory("yahoo")
@@ -39,13 +29,12 @@ def test_pipeline_yahoo_data():
     normalized_file = normalizer.normalized_file
     assert os.path.exists(normalized_file), "Normalized file not saved."
 
-def test_pipeline_wsj_data():
+def test_wsj_pipeline():
 
     downloader = GainerDownloadWSJ()
     downloader.download()
 
     raw_file = "wgainers.csv"
-=======
     raw_file = get_yahoo_gainers()
     normalized_file = normalize_csv(raw_file)
 
@@ -57,16 +46,3 @@ def test_pipeline_wsj_data():
     assert 'price_percent_change' in df.columns
     assert df['price_percent_change'].dtype == float
 
-def test_pipeline_wsj_data():
-
-    raw_file = get_wsj_gainers()
->>>>>>> fa44f416df49de1a81d577f446138af5685a2c8c
-    normalized_file = normalize_csv(raw_file)
-
-    df = pd.read_csv(normalized_file)
-
-    # Basic checks
-    assert not df.empty
-    assert 'symbol' in df.columns
-    assert 'price_percent_change' in df.columns
-    assert df['price_percent_change'].dtype == float
